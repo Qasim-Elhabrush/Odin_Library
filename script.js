70dedf65005fd5cbb33f2f27f68d3215609614eb
Qasim-Elhabrush <qasimelhabrush@gmail.com>
2022-07-22 01:36:18 -0400

    Utilizes Classes for creating books/new forms

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ let myLibrary = [];
 let newBook = "";
 let form = document.getElementById("formContainer");
 
-function Book() {
+class NewForm{
+    constructor(){
+    document.getElementById("bookTitle").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("yes").checked = false;
+    document.getElementById("no").checked = false;
+    document.getElementById("yesRecommend").checked = false;
+    document.getElementById("notRecommend").checked = false;}
+
+}
+class Book{
+    constructor(){
     this.title = document.getElementById("bookTitle").value.trim();
     this.author = document.getElementById("author").value.trim();
     this.pages = document.getElementById("pages").value.trim();
@@ -19,32 +31,41 @@ function Book() {
         this.recommend = "";
     }
     this.info = `${this.title}\nby: ${this.author}\npages: ${this.pages}\n ${this.read}\n${this.recommend}`;
+    }
+    addBookToLibrary(){
+        myLibrary.push();
+        this.updateLibrary();
+    }
+    updateLibrary() {
+        if (myLibrary.length <= 8) {
+            let divs = document.createElement("div");
+            divs.innerText = this.info;
+            divs.className = "book";
+            document.getElementById("bookContainer").appendChild(divs);
+        }
+    }    
+
 }
-const addBookToLibrary = function(book) {
-    myLibrary.push(book);
-    updateLibrary(book);
+const submitBook = () => {
+    newBook = document.getElementById("bookTitle").innerText;
+    newBook=new Book();
+    if (newBook.title != "" && newBook.author != "" && newBook.pages != "") {
+        newBook.addBookToLibrary();
+    }
+    new NewForm();
+    
+
 }
+
+
+
 const openForm = function() {
     form.style.display = "flex";
 }
 const closeForm = function() {
     form.style.display = "none";
 }
-const submitBook = function() {
-    newBook = document.getElementById("bookTitle").innerText;
-    newBook = new Book();
-    if (newBook.title != "" && newBook.author != "" && newBook.pages != "") {
-        addBookToLibrary(newBook);
-    }
-    document.getElementById("bookTitle").value = "";
-    document.getElementById("author").value = "";
-    document.getElementById("pages").value = "";
-    document.getElementById("yes").checked = false;
-    document.getElementById("no").checked = false;
-    document.getElementById("yesRecommend").checked = false;
-    document.getElementById("notRecommend").checked = false;
 
-}
 let addBookButton = document.getElementById("addButton");
 let cancelButton = document.getElementById("cancelButton");
 let submitButton = document.getElementById("submit");
@@ -55,11 +76,3 @@ addBookButton.addEventListener("click", openForm);
 addBookButton.addEventListener("mousedown", () => addBookButton.style.backgroundColor = "rgba(30, 240, 170, 0.8)");
 addBookButton.addEventListener("mouseup", () => addBookButton.style.backgroundColor = "rgb(72, 216, 154)");
 console.log(document.getElementById("bookTitle").innerText);
-const updateLibrary = function(book) {
-    if (myLibrary.length <= 8) {
-        let divs = document.createElement("div");
-        divs.innerText = book.info;
-        divs.className = "book";
-        document.getElementById("bookContainer").appendChild(divs);
-    }
-}
